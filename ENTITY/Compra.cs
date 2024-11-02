210a1de9ca4e242aaf6a171e476f40a170fad316
s1cboness3 <juandavidpenuelaperez24@gmail.com>
2024-11-01 21:54:07 -0500

    Agrego la entidad de compra

---
+++ ENTITY/Compra.cs
@@ -0,0 +1,28 @@
+﻿namespace ENTITY
+{
+    public class Compra
+    {
+
+        public Compra() { }
+
+        public Compra(int idcompra, Proveedor proveedor, Productos productos, int cantidad, decimal preciocompra)
+        {
+            IDCompra = idcompra;
+            Proveedor = proveedor;
+            Productos = productos;
+            Cantidad = Cantidad;
+            PrecioCompra = PrecioCompra;
+        }
+
+        public int IDCompra { get; set; }
+        public Proveedor Proveedor { get; set; }
+        public Productos Productos { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioCompra { get; set; }
+
+        public override string ToString()
+        {
+            return $"{IDCompra},{Proveedor.IDProveedor},{Productos.IDProducto},{Cantidad},{PrecioCompra}"; 
+        }
+    }
+}
